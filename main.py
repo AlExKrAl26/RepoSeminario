0ed0537219f6ab60c0d676af29f7e5a1faf18ec7
Alex Guillen <ajhon3222@gmail.com>
2024-05-02 15:48:55 -0500

    SE AGREGO LA CALCUALDORA

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ a = 5
 b = 6
 print("La Suma es: ",a + b)
 print("La Resta es:",a-b)
+# Se solicitan dos números al usuario
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+# Se realizan las operaciones matemáticas
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+# Se muestran los resultados al usuario
+print(f"La suma de {numero1} y {numero2} es: {suma}")
+print(f"La resta de {numero1} y {numero2} es: {resta}")
+print(f"La multiplicación de {numero1} y {numero2} es: {multiplicacion}")
+print(f"La división de {numero1} y {numero2} es: {division}")
